c75749402eee7cbd892864ac7d312b7afd4230da
Shimshon21 <shimpolak@gmail.com>
2018-05-20 23:02:41 +0300

    First commit.

---
+++ Number Wizard UI/Assets/LevelManager.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LevelManager : MonoBehaviour {
+
+    public void loadLevel(string levelName)
+    {
+        Debug.Log("Level load requested for " + levelName);
+        Application.LoadLevel(levelName);
+    }
+    public void quitRequest()
+    {
+        Debug.Log("Exit from game thanks for playing!");
+        Application.Quit();
+    }
+	
+}
---
+++ Number Wizard UI/Assets/NumberGaussed.cs
@@ -0,0 +1,53 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine.UI;
+using UnityEngine;
+
+public class NumberGaussed : MonoBehaviour {
+    public Text numberGaussed;
+    int max = 100, min = 0, numGuess = 0;
+    int numberOfTries = 10;
+
+    // Use this for initialization
+     void Start()
+    {
+        numGuess = Random.Range(0, 100);
+        print(numberGaussed);
+        numberGaussed.text = numGuess.ToString();//Nullpointer exception but still change the text..
+        print(numGuess);
+        print("Start Method called !!!");
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+      
+    }
+    public void higherNum(Text numberGaussed)
+    {
+        print("HigherNum Method called !!!");
+        if (numberOfTries != 0)
+        {
+            min = numGuess;
+            numGuess = Random.Range(min,max);
+            numberGaussed.text = numGuess.ToString();
+            numberOfTries--;
+        } else{Application.LoadLevel("Win");
+        }
+    }
+
+    public void lowerNum(Text numberGaussed)
+    {
+        print("LowerNum Method called !!!" + numGuess);
+        if (numberOfTries != 0)
+        {
+            print("The guessed number is " + numGuess);
+            max = numGuess;
+            print("Min:" + min + " to Max:" + max);
+            numGuess = Random.Range(min, max);
+            numberGaussed.text = numGuess.ToString();
+            numberOfTries--;
+        }
+        else { Application.LoadLevel("Win");}
+    }
+}
